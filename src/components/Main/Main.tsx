648378a09fdc96b61f329cc590f3ff44054a1bae
MaxYeromenko <eromenko.max@gmail.com>
2025-08-08 16:48:20 +0300

    a

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,30 +3,19 @@ import getSpotifyToken from "../../ts/GetSpotifyAccessToken";
 // import { createMusicFetcher } from "../../ts/musicApi";
 
 export default function Main() {
-    // const searchDeezer = createMusicFetcher("deezer");
-    // const searchJamendo = createMusicFetcher("jamendo", 1);
-
-    // (async () => {
-    //     const d = await searchDeezer("Imagine Dragons");
-    //     console.log("Deezer:", d);
-
-    //     const j = await searchJamendo("Imagine Dragons");
-    //     console.log("Jamendo:", j);
-    // })();
-    // const token = fetch("/api/spotify-token");
-    // fetch(
-    //     "https://api.spotify.com/v1/search?q=Imagine%20Dragons&type=track&limit=5",
-    //     {
-    //         headers: {
-    //             Authorization: `Bearer ${token}`,
-    //         },
-    //     }
-    // )
-    //     .then((res) => res.json())
-    //     .then((data) => console.log(data));
     async function example() {
         const token = await getSpotifyToken();
-        console.log(token);
+        const res = await fetch(
+            "https://api.spotify.com/v1/search?q=Imagine%20Dragons&type=track&limit=5",
+            {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            }
+        );
+        const data = await res.json();
+
+        console.log(token, data);
     }
 
     example();
