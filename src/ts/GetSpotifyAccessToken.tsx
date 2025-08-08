fa6382166e29c144ebc8fd45e8d65b5681475226
MaxYeromenko <eromenko.max@gmail.com>
2025-08-08 16:24:53 +0300

    a

--- src/ts/GetSpotifyAccessToken.tsx
+++ src/ts/GetSpotifyAccessToken.tsx
@@ -1,5 +1,5 @@
 export default async function getSpotifyToken() {
-    const res = await fetch("../api/spotify-token");
+    const res = await fetch("/src/api/spotify-token");
     const data = await res.json();
     return data.access_token;
 }
