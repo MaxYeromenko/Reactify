d8ddd619861d721498897fdc80e471250c7e9980
MaxYeromenko <eromenko.max@gmail.com>
2025-08-08 16:31:36 +0300

    a

--- src/api/spotify-token.js
+++ src/api/spotify-token.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+async function handler(req, res) {
     const clientId = process.env.SPOTIFY_CLIENT_ID;
     const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
