8be4165afd752d36b40a402c3e4842a978462b0d
MaxYeromenko <eromenko.max@gmail.com>
2025-08-20 01:48:24 +0300

    optimization

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { MinifyOptions } from 'terser'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      } as MinifyOptions['compress'],
+    },
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom']
+        }
+      }
+    }
+  }
 })
